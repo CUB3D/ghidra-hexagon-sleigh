49338e7fc86265b0268c11f0a36b87d64210f83b
CUB3D <callumthom11@gmail.com>
2025-02-25 19:11:10 +0000

    Merge branch 'master' of github.com:CUB3D/old_random_archive

---
+++ Hexagon/ghidra_scripts/test.py
@@ -0,0 +1,45 @@
+from ghidra.program.disassemble import Disassembler
+from ghidra.app.util import PseudoDisassembler
+
+for sel in currentSelection:
+    for addr in sel:
+        ins = getInstructionAt(addr)
+        if ins is not None:
+            ic = ins.instructionContext
+            print(ic)
+            print(ic.inputObjects)
+            if ic is not None:
+                pc = ic.processorContext
+                print(pc)
+                
+                #print(pc.registers)
+
+                hasext0 = pc.getRegister("hasext0")
+                print(hasext0)
+                hasext0 = pc.getValue(hasext0, False)
+                print(hasext0)
+
+                hasext0 = pc.getRegister("testctx")
+                print(hasext0)
+                hasext0 = pc.hasValue(hasext0)
+                print(hasext0)
+
+                dis = PseudoDisassembler(currentProgram)
+                pi = dis.disassemble(addr)
+                ic = pi.instructionContext
+                print(ic.inputObjects)
+                pc = ic.processorContext
+                print(pc)
+
+                hasext0 = pc.baseContextRegister
+                print(hasext0)
+                hasext0 = pc.hasValue(hasext0)
+                print(hasext0)
+                
+                #print(pc.registers)
+
+
+                
+                #dis = Disassembler(currentProgram, None, None)
+                #dis.disassemble(addr, None)
+                #print(dis.disassemblerContext)
