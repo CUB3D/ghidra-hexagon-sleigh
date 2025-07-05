8cddafedd10e625edcbef6faf5a72024dec758ec
CUB3D <callumthom11@gmail.com>
2025-07-05 01:42:11 +0100

    Fix cursed backref S3 dotnew encoding

--- Hexagon/src/main/java/hexagon/HexagonAnalyzer.java
+++ Hexagon/src/main/java/hexagon/HexagonAnalyzer.java
@@ -15,11 +15,26 @@
  */
 package hexagon;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import ghidra.app.plugin.core.analysis.AutoAnalysisManager;
 import ghidra.app.services.AbstractAnalyzer;
 import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.options.Options;
+import ghidra.program.disassemble.Disassembler;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressIterator;
+import ghidra.program.model.address.AddressSet;
 import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.lang.Register;
+import ghidra.program.model.listing.ContextChangeException;
+import ghidra.program.model.listing.Instruction;
+import ghidra.program.model.listing.Listing;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
@@ -28,12 +43,20 @@ import ghidra.util.task.TaskMonitor;
  * Provide class-level documentation that describes what this analyzer does.
  */
 public class HexagonAnalyzer extends AbstractAnalyzer {
+	
+	Register or1;
+	Register or2;
+	Register or3;
+	Register analysed;
 
+	Register[] regs_set = {or1, or2, or3};;
+	
 	public HexagonAnalyzer() {
 
 		// Name the analyzer and give it a description.
 
-		super("My Analyzer", "Analyzer description goes here", AnalyzerType.BYTE_ANALYZER);
+		super("Hexagon dotnew Analyzer"
+				+ "", "Analyzer description goes here", AnalyzerType.INSTRUCTION_ANALYZER);
 	}
 
 	@Override
@@ -49,7 +72,17 @@ public class HexagonAnalyzer extends AbstractAnalyzer {
 
 		// Examine 'program' to determine of this analyzer should analyze it.  Return true
 		// if it can.
+		
+		or1 = program.getProgramContext().getRegister("or1test");
+		or2 = program.getProgramContext().getRegister("or2test");
+		or3 = program.getProgramContext().getRegister("or3test");
+		analysed = program.getProgramContext().getRegister("analysed");
 
+		regs_set[0] = or1;
+		regs_set[1] = or2;
+		regs_set[2] = or3;
+
+		
 		return true;
 	}
 
@@ -64,11 +97,117 @@ public class HexagonAnalyzer extends AbstractAnalyzer {
 
 	@Override
 	public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log)
-			throws CancelledException {
+			throws CancelledException{
+		
+		final long locationCount = set.getNumAddresses();
+		monitor.initialize(locationCount);
+		
+		AddressIterator addresses = set.getAddresses(true);
+		
+		long count = 0;
+		
+		Listing list = program.getListing();
+		
+//		for(Register r : program.getProgramContext().getContextRegisters()) {
+//
+//			log.appendMsg("" + r);
+//		}
+		
+		
+		
+//		log.appendMsg("" + or1);
+//		log.appendMsg("" + or2);
+//		log.appendMsg("" + or3);
+		
+		
+//		
+//		
+	
+		
+		while (addresses.hasNext()) {
+			monitor.checkCancelled();
+
+			Address addr = addresses.next();
+			
+			BigInteger old_or1 = program.getProgramContext().getValue(analysed, addr, false);
+			if(old_or1 != null) {
+				if(old_or1.intValue() != 0) {
+					continue;
+				}
+			}
+
+			monitor.setProgress(count);
+			count += 1;
+
+			Instruction inst = list.getInstructionAt(addr);
+			if (inst == null) {
+				continue;
+			}
+			
+			String ins = inst.toString();
+			// rX | rXX | LR | FP | SP
+			Pattern regs = Pattern.compile("(r\\d{1,2})|LR|FP|SP\\s*=");
+			Matcher m = regs.matcher(ins);
+//			log.appendMsg(ins);
+//			System.out.println(ins);
+			int idx = 0;
+			List<Integer> outvals = new ArrayList<>();
+			while(m.find()) {
+				String g = m.group();
+//				log.appendMsg(g);
+//				System.out.println(g);
+				g = g.replaceAll("=", "");
+				g = g.strip();
+				
+				if(g.startsWith("r")) {
+					String num = g.substring(1);
+//					log.appendMsg(""+num);
+//					System.out.println(""+num);
+					int val = Integer.parseInt(num);
+					val *= 4;
+					log.appendMsg(""+val);
+					System.out.println(""+val);
+										
+					outvals.add(val);
+					idx += 1;
+				}
+			}
+			
+			program.getListing().clearCodeUnits(addr, addr.add(2), true);
+			
+			try {
+				program.getProgramContext().setValue(analysed, addr, addr, new BigInteger("1"));
+			} catch (ContextChangeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+			for(int idx2 = 0; idx2 < idx; idx2++) {
+				if(idx2 < 3) {
+					int val = outvals.get(idx2);
+					try {
+						program.getProgramContext().setValue(regs_set[idx2], addr, addr, new BigInteger(""+val));
+					} catch (ContextChangeException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			Disassembler dis = Disassembler.getDisassembler(program, monitor, null);
+			AddressSet disassembled = dis.disassemble(addr, null, false);
+			if (!disassembled.contains(addr)) {
+				// give up, the instruction couldn't be disassembled
+				return false;
+			}
+
+			AutoAnalysisManager.getAnalysisManager(program).codeDefined(disassembled);
+		}
 
 		// Perform analysis when things get added to the 'program'.  Return true if the
 		// analysis succeeded.
 
-		return false;
+		return true;
 	}
 }
--- Hexagon/test_files/test.c
+++ Hexagon/test_files/test.c
@@ -1,16 +1,25 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 
-int foo() {} 
+extern int rand();
+volatile int nope = 0;
+int foo() {
+    int i = 0;
+    while(nope) {
+        i += 1;
+    }
+    return i;
+} 
 
 int test_s3_new() {
-   asm("{ if(cmp.eq(r4.NEW, r4)) jump:nt foo; r4 = #10; r5=r4; } ");
+   //asm("{ if(cmp.eq(r4.NEW, r4)) jump:nt foo; r4 = #10; r5=r4; } ");
 
-   asm("{ if(cmp.eq(r0.NEW, #0)) jump:nt foo; r0 = #10; } ");
-   asm("{ if(cmp.eq(r1.NEW, #0)) jump:nt foo; r1 = #10; } ");
+   //asm("{ if(cmp.eq(r0.NEW, #0)) jump:nt foo; r0 = #10; } ");
+   asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r28 = #10; } ");
+   /*asm("{ if(cmp.eq(r1.NEW, #0)) jump:nt foo; r1 = #10; } ");
    asm("{ if(cmp.eq(r2.NEW, #0)) jump:nt foo; r2 = #10; } ");
    asm("{ if(cmp.eq(r3.NEW, #0)) jump:nt foo; r3 = #10; } ");
    asm("{ if(cmp.eq(r4.NEW, #0)) jump:nt foo; r4 = #10; } ");
-   asm("{ if(cmp.eq(r5.NEW, #0)) jump:nt foo; r5 = #10; } ");
+   asm("{ if(cmp.eq(r5.NEW, #0)) jump:nt foo; r5 = #10; } ");*/
    return 0;
 }
 
