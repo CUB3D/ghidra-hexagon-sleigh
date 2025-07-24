93185e9a1978f1b3340d6e58732a4d00c4917dcc
CUB3D <callumthom11@gmail.com>
2025-07-24 06:28:41 +0100

    Fix ghidra being weird obout bulk register caching

--- Hexagon/test_files/test.c
+++ Hexagon/test_files/test.c
@@ -14,12 +14,15 @@ int test_s3_new() {
    //asm("{ if(cmp.eq(r4.NEW, r4)) jump:nt foo; r4 = #10; r5=r4; } ");
 
    //asm("{ if(cmp.eq(r0.NEW, #0)) jump:nt foo; r0 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r28 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r0=#10; r28 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r1=#20; r0=#10; r28 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #0x1)) jump:nt foo; r1=#20; r0=#10; r28 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #1)) jump:nt foo; r1=#20; r28 = #10; } ");
-   asm("{ if(cmp.eq(r28.NEW, #1)) jump:nt foo; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r0=#10; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #0)) jump:nt foo; r1=#20; r0=#10; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #0x1)) jump:nt foo; r1=#20; r0=#10; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #1)) jump:nt foo; r1=#20; r28 = #10; } ");
+   //asm("{ if(cmp.eq(r28.NEW, #1)) jump:nt foo; r28 = #10; } ");
+   //asm("{ r17:16 = combine(r0, r1); memd(SP+#-0x10) = r17:16; allocframe(#0x10); } ");
+
+   asm("{ P0 = bitsclr(r9, #0x3); if(!P0.new) r2=#0x100 ; } ");
    /*asm("{ if(cmp.eq(r1.NEW, #0)) jump:nt foo; r1 = #10; } ");
    asm("{ if(cmp.eq(r2.NEW, #0)) jump:nt foo; r2 = #10; } ");
    asm("{ if(cmp.eq(r3.NEW, #0)) jump:nt foo; r3 = #10; } ");
