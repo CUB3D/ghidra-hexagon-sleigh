b0bafc5afb29b65e1576b4e7d57a1ebb397767af
CUB3D <callumthom11@gmail.com>
2026-03-05 08:10:40 +0000

    cr

--- Hexagon/ghidra_scripts/mark_known_diag.py
+++ Hexagon/ghidra_scripts/mark_known_diag.py
@@ -6,6 +6,10 @@
 
 from ghidra.app.util.cparser.C import CParser
 from ghidra.program.model.data import DataTypeConflictHandler, ArrayDataType
+from ghidra.program.model.listing.Function import FunctionUpdateType
+from ghidra.program.model.symbol import SourceType
+from ghidra.program.model.listing import ParameterImpl, VariableStorage
+from ghidra.program.model.data import VoidDataType, PointerDataType, UnsignedIntegerDataType
 
 # Makes this much slower
 VERBOSE = False
@@ -33,7 +37,10 @@ known_diags = [
     
     ("(\\x00\\xfa){2}.{4}(\\x02\\xfa){2}", 86, "DIAG_uim"),
     ("(\\x00\\x00){2}.{4}(\\x09\\x00){2}.{4}(\\x0a\\x00){2}", 4, "DIAG_umts"),
+    
+    ("(\\x00\\x00){2}.{4}(\\x01\\x00){2}.{4}(\\x08\\x00){2}.{4}", 15, "DIAG_tdscdma"),
 ]
+known_diags = known_diags[-1:]
 
 transaction = dtm.startTransaction("Adding qcom_diag_ent struct")
 dtm.addDataType(diag_ent, None)
@@ -42,13 +49,26 @@ dtm.endTransaction(transaction, True)
 start_address = currentProgram.getMinAddress()
 
 for (pattern, size, name) in known_diags:
-    matches = findBytes(start_address, pattern, 1, 4)
-    if not matches:
-        print name, "Not found"
-        continue
-    pos = matches[0]
-    print "Found", name, "@", pos
-    clearListing(pos, pos.add(8 * (size - 1)+4))
-    createLabel(pos, name, True)
-    array_datatype = ArrayDataType(diag_ent, size)
-    createData(pos, array_datatype)
+    try:
+        matches = findBytes(start_address, pattern, 1, 4)
+        if not matches:
+            print name, "Not found"
+            continue
+        pos = matches[0]
+        print "Found", name, "@", pos
+        clearListing(pos, pos.add(8 * (size - 1)+4))
+        createLabel(pos, name, True)
+        array_datatype = ArrayDataType(diag_ent, size)
+        createData(pos, array_datatype)
+        
+        for ent_idx in range(size):
+            ent_addr = pos.add(8*ent_idx+4)
+            addr = getInt(ent_addr)
+            func = createFunction(toAddr(addr), None)            
+            func.replaceParameters(FunctionUpdateType.CUSTOM_STORAGE, False, SourceType.IMPORTED, [
+                ParameterImpl("pkt", PointerDataType(VoidDataType.dataType), VariableStorage.deserialize(currentProgram, "register:00000000:4"), currentProgram, SourceType.IMPORTED),
+                ParameterImpl("len", UnsignedIntegerDataType.dataType, VariableStorage.deserialize(currentProgram, "register:00000004:4"), currentProgram, SourceType.IMPORTED)
+            ])
+        
+    except Exception as e:
+        print(e)
