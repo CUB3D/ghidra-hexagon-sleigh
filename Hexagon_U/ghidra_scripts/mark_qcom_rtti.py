74c65d11c164177e77d0fff191fe458864bdc04f
CUB3D <callumthom11@gmail.com>
2026-07-10 12:21:23 +0000

    More rtti

--- Hexagon_U/ghidra_scripts/mark_qcom_rtti.py
+++ Hexagon_U/ghidra_scripts/mark_qcom_rtti.py
@@ -9,6 +9,7 @@ from ghidra.program.model.listing.Function import FunctionUpdateType
 from ghidra.program.model.symbol import SourceType
 from ghidra.program.model.listing import ParameterImpl, VariableStorage
 from ghidra.program.model.data import VoidDataType, PointerDataType, UnsignedIntegerDataType, CharDataType
+import subprocess
 
 void_ptr = PointerDataType(VoidDataType.dataType)
 
@@ -80,6 +81,52 @@ def find_num(addr):
 		r += [s.getOffset()]
 	return r
 
+
+def clean_label_str(x):
+        return x.replace("<", "_").replace(">", "_").replace(" ", "_").replace("(", "_").replace(")", "_")
+
+
+def create_or_read_string(str_addr):
+	try:
+		name = createAsciiString(str_addr).getValue()
+		if len(name) == 0:
+			return None # pointer to zero data
+		createLabel(typeinfo_base, name, True)
+	except:
+		name = getDataAt(str_addr)
+		if name is None:
+			print "BAD NAME @", str_addr
+			return
+		name = name.getValue()
+		
+		if ("unicode" not in str(type(name))) or len(name) == 0:
+			return None # pointer to random data
+        return name
+
+def clear_and_set_lbl(addr, lbl, primary=True):
+    print "set lbl", addr, lbl
+    symbol_table = currentProgram.getSymbolTable()
+    symbols = symbol_table.getSymbols(addr)
+    
+    for symbol in symbols:
+        symbol_table.removeSymbolSpecial(symbol)
+
+    createLabel(addr, clean_label_str(lbl), primary)
+
+
+def is_addr_mapped(x):
+        try:
+                getLong(x)
+                return True
+        except:
+                return False
+
+
+def demangle(name):
+    x = subprocess.check_output(['c++filt', "_Z" + name])
+    print(x)
+    return x.strip()
+
 def mark_vtable(x, name):
 	print "VT @ ", x 
 	try:
@@ -94,14 +141,15 @@ def mark_vtable(x, name):
 		createData(x.add(4), void_ptr)
 	except:
 		pass
-	createLabel(x, name + "_typeinfo", True)
-	createLabel(x.add(4), name + "_vtable", True)
+
+	clear_and_set_lbl(x, name + "_typeinfo", True)
+	clear_and_set_lbl(x.add(4), name + "_vtable", True)
 	i = 4
 	while True:
 		ptr = getLong(x.add(i)) & 0xFFFFFFFF
 		if ptr == 0:
 			break
-		if not toAddr(ptr).isLoadedMemoryAddress():
+		if not is_addr_mapped(toAddr(ptr)):
 			break
 		try:
 			clearListing(x.add(i), x.add(i).add(4))
@@ -119,35 +167,21 @@ def mark_typeinfo(x, name, indent=1):
 	# This is the vtable
 	if getLong(x.add(-4)) & 0xFFFFFFFF == 0:
 		mark_vtable(x, name)
-		return
-	# Some things sneak past that
-	if getLong(x.add(-4)) & 0xFFFFFFFF < 0x01000000:
+
+	if is_addr_mapped(toAddr(getLong(x.add(-4)) & 0xFFFFFFFF)):
 		return
 	#print(x)
 	typeinfo_base = x.add(-8)
 	
 	str_addr = toAddr(getLong(typeinfo_base.add(4)) & 0xFFFFFFFF)
-	#print("S", str_addr)
-	try:
-		name = createAsciiString(str_addr).getValue()
-		if len(name) == 0:
-			return # pointer to random data
-		createLabel(typeinfo_base, name, True)
-	except:
-		name = getDataAt(str_addr)
-		if name is None:
-			print("BAD NAME @", str_addr)
-			return
-		name = name.getValue()
-		#print(name)
-		#print(type(name))
-		
-		if ("unicode" not in str(type(name))) or len(name) == 0:
-			return # pointer to random data
-		try:
-			createLabel(typeinfo_base, name, True)
-		except:
-			return # name isn't a string?
+        if not is_addr_mapped(str_addr):
+                return
+        name = create_or_read_string(str_addr)
+        if name is None or len(name) == 0:
+                return
+
+        demangled_name = demangle(name)
+        clear_and_set_lbl(typeinfo_base, demangled_name, True)
 	
 	print("- " * indent, x, "=", name)
 	
@@ -155,13 +189,13 @@ def mark_typeinfo(x, name, indent=1):
 	createData(typeinfo_base, cxx_type_info)
 	
 	for y in find_num(typeinfo_base.getOffset()):
-		mark_typeinfo(toAddr(y), name, indent + 1)
+		mark_typeinfo(toAddr(y), demangled_name, indent + 1)
 		
 		
 	vtbl_addr = toAddr(getLong(typeinfo_base) & 0xFFFFFFFF)
 
 	for x in find_num(vtbl_addr.getOffset()):
-		mark_typeinfo_vtbl_ref(toAddr(x), name)
+		mark_typeinfo_vtbl_ref(toAddr(x), demangled_name)
 
 def mark_typeinfo_vtbl_ref(x, name):
 	
@@ -178,24 +212,19 @@ def mark_typeinfo_vtbl_ref(x, name):
 	v5 = getLong(x.add(16)) & 0xFFFFFFFF 
 	
 	str_addr = toAddr(getLong(x.add(4)) & 0xFFFFFFFF)
-	try:
-		name = createAsciiString(str_addr).getValue()
-		if len(name) == 0:
-			return # pointer to random data
-		createLabel(x, name, True)
-	except:
-		name = getDataAt(str_addr)
-		if name is None:
-			return
-		name = name.getValue()
-		if ("unicode" not in str(type(name))) or len(name) == 0:
-			return # pointer to random data
-		try:
-			createLabel(x, name, True)
-		except:
-			return # name isn't a string?
+        if not is_addr_mapped(str_addr):
+                return
+        name = create_or_read_string(str_addr)
+        if name is None or len(name) == 0:
+                return
+        demangled_name = demangle(name)
+        clear_and_set_lbl(x, demangled_name, True)
+
+
+        if (v1 == 0) or (v2 == 0) or (not is_addr_mapped(toAddr(v1))) or (not is_addr_mapped(toAddr(v2))):
+                return
 	
-	if (v5 == 0) or (v4 == 0) or toAddr(v5).isNonLoadedMemoryAddress() or toAddr(v4).isNonLoadedMemoryAddress():
+	if (v5 == 0) or (v4 == 0) or (not is_addr_mapped(toAddr(v5))) or (not is_addr_mapped(toAddr(v4))):
 		 kind = cxx_type_info_vtbl_ref_short
 		 clearListing(x, x.add(4*3))
 	else:
@@ -208,33 +237,36 @@ def mark_typeinfo_vtbl_ref(x, name):
 		pass
 		
 	for x in find_num(x.getOffset()):
-		mark_typeinfo_vtbl_ref(toAddr(x), name)
+		mark_typeinfo_vtbl_ref(toAddr(x), demangled_name)
 
 def mark_typeinfo_root(name):
 	name_str = str(name[:-1])
-	print("---", name_str, "---")
+        demangled_name = demangle(name_str)
+	print("---", demangled_name, "---")
 	type_info_root_name = find_str(name)
 	type_info_root = toAddr(find_num_single(type_info_root_name)).add(-4)
 	clearListing(type_info_root, type_info_root.add(8))
 	createData(type_info_root, cxx_type_info_root)
-	createLabel(type_info_root, name_str+"_type_info", True)
+	clear_and_set_lbl(type_info_root, demangled_name+"_type_info", True)
 	
 	vtbl_addr = toAddr(getLong(type_info_root) & 0xFFFFFFFF)
-	createLabel(vtbl_addr, name_str+"_typeinfo_vtable", True)
+	clear_and_set_lbl(vtbl_addr, demangled_name+"_typeinfo_vtable", True)
 	
 	for x in find_num(type_info_root.getOffset()):
-		mark_typeinfo(toAddr(x), name_str)
+		mark_typeinfo(toAddr(x), demangled_name)
 		
 	for x in find_num(vtbl_addr.getOffset()):
-		mark_typeinfo_vtbl_ref(toAddr(x), name_str)
+		mark_typeinfo_vtbl_ref(toAddr(x), demangled_name)
 		
 	print(name_str, "@", type_info_root)
 
 
-mark_typeinfo_root(b"St9type_info\0")
+#mark_typeinfo_root(b"St9type_info\0")
 mark_typeinfo_root(b"8MafTimer\0")
 
+
+
 #TODO: more roots
-#TODO: remove old labels
 #TODO: demangle names
+
 
